2dcfdd7418394a9d9d09528bd8a484ed037cc25b
Tina Shi <init.defaultBranch>
2023-03-29 16:40:11 +1100

    Updated js

--- index.js
+++ index.js
@@ -13,81 +13,93 @@
 //         document.getElementById("villagersList").appendChild(node);    
 //     })
 // }
-const villagersList = document.getElementById('villagersList');
-const fetchVillagers = () => {
-    const villagers = [];
-    for (let i=1; i < 50; i++) {
-        const url = `https://acnhapi.com/v1/villagers/${i}`
-        villagers.push(fetch(url).then((res) => res.json()))
+document.addEventListener('DOMContentLoaded', initialize)
+
+function initialize() {
+    const villagersList = document.getElementById('villagersList');
+    const fetchVillagers = () => {
+        const villagers = [];
+        for (let i=1; i <= 50; i++) {
+            const url = `https://acnhapi.com/v1/villagers/${i}`
+            villagers.push(fetch(url).then((res) => res.json()))
+        }
+        Promise.all(villagers).then((results) => {
+            const villager = results.map((result) => ({
+                name: result.name["name-USen"],
+                image: result["icon_uri"],
+                personality: result.personality
+            }));
+            displayVillagers(villager);
+        })
     }
-    Promise.all(villagers).then((results) => {
-        const villager = results.map((result) => ({
-            name: result.name["name-USen"],
-            image: result["icon_uri"],
-            personality: result.personality
-        }));
-        displayVillagers(villager);
-    })
-}
 
-const displayVillagers = (villager) => {
-    console.log(villager);
-    const villagerHTMLString = villager
-        .map(
-            (village) => `
-            <li class="villager">
-            <img src="${village.image}" />
-            <h3 class="villagerClass"> ${village.name}</h3>
-            <p>Personality: ${village.personality}</p>
-        </li>  
-    `
-        )
-        .join('');
-    villagersList.innerHTML = villagerHTMLString;
-};
+    const displayVillagers = (villager) => {
+        // console.log(villager);
+        const villagerHTMLString = villager
+            .map(
+                (village) => `
+                <li class="villager">
+                <img src="${village.image}" />
+                <h3 class="name"> ${village.name}</h3>
+                <p>Personality: ${village.personality}</p>
+                
+            </li>  
+        `
+            )
+            .join('');
+        villagersList.innerHTML = villagerHTMLString;
+    };
 
-fetchVillagers();
+    fetchVillagers();
 
-function searchVillagers() {
-    let input = document.getElementById('search').value
-    input.toLowerCase();
-    let x = document.getElementsByClassName('villagerClass'); 
+    let x = document.getElementsByClassName('name'); 
     let y = document.getElementsByClassName('villager');
-    console.log(x[1])
-    for (i = 0; i < x.length; i++) {  
-        if (!x[i].innerHTML.toLowerCase().includes(input)) { 
-            y[i].style.display="none"; 
-
-        } 
-        else { 
-            y[i].style.display="list-item";                  
+    function searchVillagers() {
+        let input = document.getElementById('search').value
+        input.toLowerCase();
+        
+        for (i = 1; i < x.length; i++) {  
+            if (!x[i].innerHTML.toLowerCase().includes(input)) { 
+                y[i].style.display="none"; 
+            } 
+            else { 
+                y[i].style.display="list-item";                  
+            } 
         } 
     } 
-} 
-
-document.getElementById("search").addEventListener("keyup", searchVillagers);
-
-
-
-
-// let input = document.getElementById("search").value;
-
-// input.addEventListener("keyup", searchVillager);    
+    let ul = document.querySelector("#villagersList")
+    document.getElementById("search").addEventListener("keyup", searchVillagers);
     
+    ul.addEventListener("click", (e) => {
+        let event = e.target.closest("LI")
+        event.style.backgroundColor = "yellow"
+    })
 
-// for (i=1; i < 50; i++) {
-//     const img = document.createElement("img")
-//     img.setAttribute("src", `https://acnhapi.com/v1/icons/villagers/${i}`)
-//     document.getElementById("villagersList").appendChild(img)
-// }
+    document.body.addEventListener("dblclick", (e) => {
+        e.target.style.backgroundColor = "null"
+    })
+    
+   
 
-// const searchInput = document.getElementById('search')
-// const villagersFromDOM = document.getElementsByTagName('p')
-// console.log(villagersFromDOM)
-// searchInput.addEventListener("input", (e) => {
-//     let value = e.target.value
-//     if (value && value.trim().length > 0) {
-//         value = value.trim().toLowerCase()
-//     }
-// })
+    // let input = document.getElementById("search").value;
 
+    // input.addEventListener("keyup", searchVillager);    
+        
+
+    // for (i=1; i < 50; i++) {
+    //     const img = document.createElement("img")
+    //     img.setAttribute("src", `https://acnhapi.com/v1/icons/villagers/${i}`)
+    //     document.getElementById("villagersList").appendChild(img)
+    // }
+
+    // const searchInput = document.getElementById('search')
+    // const villagersFromDOM = document.getElementsByTagName('p')
+    // console.log(villagersFromDOM)
+    // searchInput.addEventListener("input", (e) => {
+    //     let value = e.target.value
+    //     if (value && value.trim().length > 0) {
+    //         value = value.trim().toLowerCase()
+    //     }
+    // })
+
+}
